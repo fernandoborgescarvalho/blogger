d83e7115eff39db1a75434bb6085d6fea7c2c588
fernando borges <fernandoborgescarvalho@gmail.com>
2019-04-13 09:44:51 -0400

    Servidor embarcado Heroku

---
+++ src/main/java/br/com/blogger/controller/IndexController.java
@@ -0,0 +1,13 @@
+package br.com.blogger.controller;
+
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class IndexController implements Serializable {
+
+    public IndexController() {
+    }
+}
